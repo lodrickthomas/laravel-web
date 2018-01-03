ad7d0a1d5fc166ae5cc994a601d1aa8301d6c6ba
lodrickthomas <type_your_email_here>
2018-01-03 14:54:21 +0300

    Fifth commit, added animate.css and minor corrections on #small-words!

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,16 +5,16 @@
 @section('content')
 
   <section class="title-middle">
-    <p id="large-words" >  This is <span id="lod">Lodrick</span></p>
-    <p id="small-words">An Engineer, A programmer, and a persom who deeply believe in passion!</p>
+    <p id="large-words" class="lead animated slideInLeft">  This is <span id="lod">Lodrick</span></p>
+    <p id="small-words" class="animated slideInRight">An Engineer, A programmer, and a person who deeply believe in passion!</p>
 
-    <div class="network_links">
+    <div class="network_links animated slideInDown">
       <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
         <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></i></a>
         <a href="#"><i class="fa fa-twitter-square" aria-hidden="true"></i></a>
         <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
         <a href="#"><i class="fa fa-github-square" aria-hidden="true"></i></a>
-
+        <a href="#"><i class="fa fa-youtube-square" aria-hidden="true"></i></i></a>
     </div>
   </section>
 @endsection
